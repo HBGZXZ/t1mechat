7696ff7b87bf827f97f14675970df2bbfd28c9da
HBGZXZ <3191543667@qq.com>
2026-08-25 21:31:27 +0800

    t1Mechat: simple, pure chat

---
+++ public/sw.js
@@ -0,0 +1,44 @@
+const CACHE = "t1mechat-v1";
+const ASSETS = [
+  "/",
+  "/index.html",
+  "/app.js",
+  "/style.css",
+  "/manifest.json",
+  "/icon.svg",
+  "/site/index.html",
+  "/site/help.html",
+  "/site/privacy.html",
+  "/site/terms.html"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  const url = new URL(e.request.url);
+  if (url.protocol === "ws:" || url.protocol === "wss:") return;
+  e.respondWith(
+    caches.match(e.request).then(hit => {
+      if (hit) return hit;
+      return fetch(e.request).then(res => {
+        if (res.ok && url.origin === location.origin) {
+          const copy = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => caches.match("/"));
+    })
+  );
+});
